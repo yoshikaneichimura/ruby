ddbeb12d1ae4dd66184d2502da905b951af26f77
EC2 Default User <ec2-user@ip-172-31-36-149.ap-northeast-1.compute.internal>
2023-03-22 12:25:02 +0000

    [Add]Fizz_Buzz

--- fuzz_buzz.rb
+++ fuzz_buzz.rb
@@ -1,5 +1,13 @@
 def fizz_buzz(number)
-
+    if number % 15 == 0
+        "FizzBuzz"
+    elsif number % 3 == 0
+        "Fizz"
+    elsif number % 5 == 0
+        "Buzz"
+    else
+        number.to_s
+    end
 end
 
 puts "数字を入力して下さい"
@@ -7,4 +15,4 @@ puts "数字を入力して下さい"
 input = gets.to_i
 
 puts "結果は..."
-puts fuzz_buzz(input)
+puts fizz_buzz(input)
